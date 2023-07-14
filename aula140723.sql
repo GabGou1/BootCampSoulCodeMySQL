b9af515b127b8c3e4ddc8e112201e5cc81b26254
GabGou1 <gabrielriberikoa@gmail.com>
2023-07-14 14:33:16 -0300

    Aula do dia 140723

--- aula140723.sql
+++ aula140723.sql
@@ -2,6 +2,7 @@ CREATE DATABASE loja_db;
 
 USE loja_db;
 
+-- Schema => Estrutura de tabelas e colunas
 CREATE TABLE vendedor(
 	idVendedor INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(60) NOT NULL,
@@ -9,3 +10,10 @@ CREATE TABLE vendedor(
     email VARCHAR(100) UNIQUE NOT NULL,
     dataNascimento DATE
 );
+
+DROP TABLE vendedor; -- Apaga todos os registros e o Scheam da tabela
+
+-- categoria - VARCHAR(30) NOT NULL
+
+ALTER TABLE vendedor
+ADD COLUMN cat VARCHAR(30) NOT NULL DEFAULT("Sem categoria");
